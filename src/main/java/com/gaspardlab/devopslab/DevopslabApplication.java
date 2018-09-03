aa2d60bfdf3e13027131ee1fd193b5838f7700f2
Gaspard Nzamurambaho <gnzamura@hotmail.com>
2018-09-03 17:21:21 +0100

    Initial Commit from IDE

---
+++ src/main/java/com/gaspardlab/devopslab/DevopslabApplication.java
@@ -0,0 +1,12 @@
+package com.gaspardlab.devopslab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopslabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevopslabApplication.class, args);
+    }
+}
